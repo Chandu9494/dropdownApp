78175905827bf79e71cc9c5a0fc7ed72ca3faa83
Chandu9494 <chandanabalakrishnan11@gmail.com>
2023-09-04 17:36:23 +0530

    Initial changes for test

---
+++ src/app/dropdown/dropdown.component.ts
@@ -0,0 +1,70 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { IOption } from './dropdown-options.interface';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.scss']
+})
+export class DropdownComponent implements OnInit {
+ public readonly options : IOption[]= [
+    {
+      id: 1,
+      label: "Option 1"
+    },
+    {
+      id: 2,
+      label: "Option 2"
+    },
+    {
+      id: 3,
+      label: "Option 3"
+    },
+    {
+      id: 4,
+      label: "Option 4"
+    },
+    {
+      id: 5,
+      label: "Option 5"
+    },
+    {
+      id: 6,
+      label: "Option 6"
+    },
+    {
+      id: 7,
+      label: "Option 7"
+    }, 
+    {
+      id: 8,
+      label: "Option 8"
+    },
+    {
+      id: 9,
+      label: "Option 9"
+    }
+
+  ];
+
+  public optionId : number = 0;
+  public placeholder!:string;
+  public selectedOption!:any;
+  public panelOpened = false;
+  @Output() onSelected = new EventEmitter<number>;
+
+  ngOnInit(): void {
+    this.placeholder = 'Select an option';
+  }
+
+  public showOptionSelected(){
+    this.panelOpened = !this.panelOpened
+  }
+
+  public handleOptionChange(option: any): void{
+    this.onSelected.emit(option.label);
+    this.selectedOption = option.label;
+    this.panelOpened = false;
+  }
+}
+
